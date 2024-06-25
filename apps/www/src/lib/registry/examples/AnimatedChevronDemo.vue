03c37b3734f9698f9964c707d1d2a25cbdc0381e
Carlos Marques <psycarlo1@gmail.com>
2024-06-25 15:12:06 +0100

    chore: update animated chevron

--- apps/www/src/lib/registry/examples/AnimatedChevronDemo.vue
+++ apps/www/src/lib/registry/examples/AnimatedChevronDemo.vue
@@ -1,10 +1,16 @@
 <template>
-  <button class="group flex items-center gap-1">
-    <span>Hover me</span>
-    <ChevronRight />
-  </button>
+  <div class="flex flex-col gap-3">
+    <button class="group flex items-center gap-1">
+      <span>Hover me</span>
+      <AnimatedChevron />
+    </button>
+    <button class="group flex items-center gap-1">
+      <AnimatedChevron direction="left" />
+      <span>And me</span>
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
-  import ChevronRight from '../ui/AnimatedChevron.vue'
+  import AnimatedChevron from '../ui/AnimatedChevron.vue'
 </script>
--- apps/www/src/lib/registry/ui/AnimatedChevron.vue
+++ apps/www/src/lib/registry/ui/AnimatedChevron.vue
@@ -1,12 +1,17 @@
 <template>
   <svg
-    class="-mr-1 ml-1.5 stroke-[1.5px]"
+    class="stroke-[1.5px]"
+    :class="{
+      '-mr-1 ml-1.5': props.direction === 'right',
+      '-ml-1 mr-1.5': props.direction === 'left'
+    }"
     fill="none"
     stroke="currentColor"
     width="11"
     height="11"
     viewBox="0 0 10 10"
     aria-hidden="true"
+    :transform="props.direction === 'left' ? 'matrix(-1,0,0,1,0,0)' : undefined"
   >
     <path
       class="opacity-0 transition group-hover:opacity-100"
@@ -18,3 +23,13 @@
     ></path>
   </svg>
 </template>
+
+<script setup lang="ts">
+  type AnimatedChevronProps = {
+    direction: 'left' | 'right'
+  }
+
+  const props = withDefaults(defineProps<AnimatedChevronProps>(), {
+    direction: 'right'
+  })
+</script>
