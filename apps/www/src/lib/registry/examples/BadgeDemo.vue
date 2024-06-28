b9be4fe22379b126e1881debf23a5c60c77438b9
Carlos Marques <psycarlo1@gmail.com>
2024-06-28 03:21:26 +0100

    feat: badge variants

--- apps/www/src/lib/registry/examples/BadgeDemo.vue
+++ apps/www/src/lib/registry/examples/BadgeDemo.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="flex gap-4">
-    <Badge class="border-green-500 bg-green-500 font-medium text-green-500">
-      High priority
-    </Badge>
-    <Badge class="border-purple-600 bg-purple-600 text-purple-600">
-      9 sat/vB
-    </Badge>
-    <Badge class="border-red-600 bg-red-600 text-red-600">
-      <ArrowDownRightIcon class="h-4 w-4" />
-      6.9%
-    </Badge>
+  <div class="flex flex-col items-center gap-4">
+    <div class="flex gap-4">
+      <Badge class="border-green-500 bg-green-500 font-medium text-green-500">
+        High priority
+      </Badge>
+      <Badge class="border-purple-600 bg-purple-600 text-purple-600">
+        9 sat/vB
+      </Badge>
+      <Badge class="border-red-600 bg-red-600 text-red-600">
+        <ArrowDownRightIcon class="h-4 w-4" />
+        6.9%
+      </Badge>
+    </div>
+    <div class="flex gap-4">
+      <Badge variant="filled" class="bg-blue-700 font-medium text-white">
+        Protected
+        <ShieldCheckIcon class="h-4 w-4 stroke-2" />
+      </Badge>
+      <Badge variant="filled" class="bg-red-600 font-medium text-white">
+        <Battery50Icon class="h-4 w-4" />
+        Battery 50%
+      </Badge>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
   import Badge from '../ui/Badge.vue'
-  import { ArrowDownRightIcon } from '@heroicons/vue/16/solid'
+  import { ArrowDownRightIcon, Battery50Icon } from '@heroicons/vue/16/solid'
+  import { ShieldCheckIcon } from '@heroicons/vue/24/outline'
 </script>
--- apps/www/src/lib/registry/ui/Badge.vue
+++ apps/www/src/lib/registry/ui/Badge.vue
@@ -9,14 +9,23 @@
   import { type HTMLAttributes, computed } from 'vue'
 
   type BadgeProps = {
+    variant?: 'default' | 'filled'
     class?: HTMLAttributes['class']
   }
 
-  const props = defineProps<BadgeProps>()
+  const props = withDefaults(defineProps<BadgeProps>(), {
+    variant: 'default'
+  })
 
   const badgeClass = computed(() =>
     tv({
-      base: 'inline-flex w-fit shrink-0 items-center gap-x-1 whitespace-nowrap rounded-md border border-opacity-20 bg-opacity-10 px-2 py-0.5 text-sm'
+      base: 'inline-flex w-fit shrink-0 items-center gap-x-1 whitespace-nowrap rounded-md px-2 py-0.5 text-sm',
+      variants: {
+        variant: {
+          default: 'border border-opacity-20 bg-opacity-10',
+          filled: ''
+        }
+      }
     })(props)
   )
 </script>
