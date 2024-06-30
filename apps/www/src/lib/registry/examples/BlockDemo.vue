574cc27c58a4d3a8ffc021958e03f95d3d972072
Carlos Marques <psycarlo1@gmail.com>
2024-06-30 21:48:39 +0100

    feat: add Block

--- apps/www/src/lib/registry/examples/BlockDemo.vue
+++ apps/www/src/lib/registry/examples/BlockDemo.vue
@@ -1,7 +1,27 @@
 <template>
-  <Block />
+  <div class="flex items-center gap-8">
+    <Block class="bg-brand-primary mt-6 size-24 animate-pulse"></Block>
+    <div class="border-brand-primary mr-4 h-24 w-1 border-l-2 border-dashed" />
+    <div class="flex flex-col items-center gap-1">
+      <span class="text-brand-secondary mb-6 mr-8 text-sm">69420</span>
+      <Block
+        class="bg-gradient bg-brand-secondary from-brand-secondaryLight to-brand-secondaryDark size-24 bg-gradient-to-b"
+      >
+        <div class="flex h-full flex-col items-center justify-center">
+          <p class="text-brand-white font-semibold">1.69MB</p>
+        </div>
+      </Block>
+      <Badge
+        variant="filled"
+        class="bg-brand-secondary text-brand-white text-xs font-medium"
+      >
+        PsyUI Pool
+      </Badge>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+  import Badge from '../ui/Badge.vue'
   import Block from '../ui/Block.vue'
 </script>
--- apps/www/src/lib/registry/ui/Block.vue
+++ apps/www/src/lib/registry/ui/Block.vue
@@ -1,3 +1,26 @@
-<template></template>
+<template>
+  <div :class="blockClass">
+    <div
+      class="absolute left-[calc(-.16*100%)] top-[calc(-.192*100%)] h-[calc(.192*100%)] w-full origin-top skew-x-[40deg] transform-gpu bg-inherit brightness-[0.85] filter"
+    ></div>
+    <div
+      class="absolute left-[calc(-.16*100%)] top-[calc(-.097*100%)] h-full w-[calc(.16*100%)] origin-top skew-y-[50deg] transform-gpu bg-inherit brightness-[1.15] filter"
+    ></div>
+    <slot />
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { tv } from 'tailwind-variants'
+  import { type HTMLAttributes, computed } from 'vue'
+
+  type BlockProps = {
+    class?: HTMLAttributes['class']
+  }
+
+  const props = defineProps<BlockProps>()
+
+  const blockClass = computed(() =>
+    tv({ base: 'bg-brand-primary relative size-32' })(props)
+  )
+</script>
