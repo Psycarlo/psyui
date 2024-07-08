8ca63e62609433a35bccaf7ce2c072652fee262c
Carlos Marques <psycarlo1@gmail.com>
2024-07-08 13:12:21 +0100

    fix: qrcode client side

--- apps/www/src/lib/registry/examples/QRCodeDemo.vue
+++ apps/www/src/lib/registry/examples/QRCodeDemo.vue
@@ -1,7 +1,13 @@
 <template>
-  <QRCode value="bitcoin:1111111111111111111114oLvT2" />
+  <ClientQRCode value="bitcoin:1111111111111111111114oLvT2" />
 </template>
 
 <script setup lang="ts">
-  import QRCode from '../ui/QRCode.vue'
+  import { defineClientComponent } from 'vitepress'
+
+  const ClientQRCode = defineClientComponent(() => {
+    return import('../ui/QRCode.vue')
+  })
+  // https://github.com/vuejs/vitepress/issues/3610
+  // Import QRCode normally if you are not using SSR.
 </script>
